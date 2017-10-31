3670328062a43b8cda3487b7d44dbd34d61661be
Hoang Van Luyen <vanhoangluyen@gmail.com>
2017-10-31 13:34:34 +0700

    Initial Commit

---
+++ SearchData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchData
+//
+//  Created by Luyen on 10/31/17.
+//  Copyright © 2017 Luyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
